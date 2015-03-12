9ddbfd262673165b4b7bb6c53bb763429d031c5e
Heather Moore <moore.hj@gmail.com>
2015-03-11 20:44:36 -0700

    deal with zero parties

--- app/views/admin/admin/index.html.rb
+++ app/views/admin/admin/index.html.rb
@@ -4,27 +4,30 @@ class Views::Admin::Admin::Index < Views::Base
   def content
     full_row {
       h1("Parties")
-      table {
-        thead {
-          parties.first.attributes.keys.each do |field|
-            th {
-              text field
-            }
-          end
+      if party = parties.first
+
+        table {
+          thead {
+            party.attributes.keys.each do |field|
+              th {
+                text field
+              }
+            end
+          }
+          tbody {
+            parties.each do |p|
+              tr {
+                p.attributes.each do |name, value|
+                  td {
+                    text value
+                  }
+                end
+              }
+            end
+          }
         }
-        tbody {
-          parties.each do |p|
-            tr {
-              p.attributes.each do |name, value|
-                td {
-                  text value
-                }
-              end
-            }
-          end
-        }
-      }
-      link_to "Download as CSV", url_for(:controller=>'admin', :action=>'index', :format=>:csv), class: "white-text"
+        link_to "Download as CSV", url_for(:controller=>'admin', :action=>'index', :format=>:csv), class: "white-text"
+      end
     }
   end
 end
